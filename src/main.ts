9af1b001f0286d2ae39ba67527f74dd2542f7c9e
imolinap9191 <imolinap91@gmail.com>
2024-05-26 21:43:28 -0300

    Permiso al Front para que acceda al back

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,17 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      forbidNonWhitelisted: true
-    })
-  )
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
